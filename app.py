d89e5446d9fc20eb75ad71dd914b91126e3c9bd2
DoniaGasmii <160879037+DoniaGasmii@users.noreply.github.com>
2025-11-18 16:39:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
 import streamlit as st
 st.title(" welcome hacker ")
+
+import base64
+
+# --- Load local video and convert to base64 ---
+def get_base64_video(video_path):
+    with open(video_path, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+video_base64 = get_base64_video("....mp4")
